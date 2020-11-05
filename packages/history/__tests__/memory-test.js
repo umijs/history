cda3d7d7ea57025d823974c648d8a2566abbd0b7
ycjcl868 <45808948@qq.com>
2020-11-05 23:07:46 +0800

    chore: memo

--- packages/history/__tests__/memory-test.js
+++ packages/history/__tests__/memory-test.js
@@ -31,7 +31,7 @@ describe('a memory history', () => {
     const href = history.createHref({
       pathname: '/the/path',
       search: '?the=query',
-      hash: '#the-hash',
+      hash: '#the-hash'
     });
 
     expect(href).toEqual('/the/path?the=query#the-hash');
@@ -44,102 +44,102 @@ describe('a memory history', () => {
 
   it('does not encode the generated path', () => {
     const encodedHref = history.createHref({
-      pathname: '/%23abc',
+      pathname: '/%23abc'
     });
     expect(encodedHref).toEqual('/%23abc');
 
     const unencodedHref = history.createHref({
-      pathname: '/#abc',
+      pathname: '/#abc'
     });
     expect(unencodedHref).toEqual('/#abc');
   });
 
   describe('listen', () => {
-    it('does not immediately call listeners', (done) => {
+    it('does not immediately call listeners', done => {
       Listen(history, done);
     });
   });
 
   describe('the initial location', () => {
-    it('has a key', (done) => {
+    it('has a key', done => {
       InitialLocationHasKey(history, done);
     });
   });
 
   describe('push a new path', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       PushNewLocation(history, done);
     });
   });
 
   describe('push the same path', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       PushSamePath(history, done);
     });
   });
 
   describe('push state', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       PushState(history, done);
     });
   });
 
   describe('push with no pathname', () => {
-    it('reuses the current location pathname', (done) => {
+    it('reuses the current location pathname', done => {
       PushMissingPathname(history, done);
     });
   });
 
   describe('push with a relative pathname', () => {
-    it('issues a warning', (done) => {
+    it('issues a warning', done => {
       PushRelativePathnameWarning(history, done);
     });
   });
 
   describe('replace a new path', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       ReplaceNewLocation(history, done);
     });
   });
 
   describe('replace the same path', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       ReplaceSamePath(history, done);
     });
   });
 
   describe('replace state', () => {
-    it('calls change listeners with the new location', (done) => {
+    it('calls change listeners with the new location', done => {
       ReplaceState(history, done);
     });
   });
 
   describe('location created with encoded/unencoded reserved characters', () => {
-    it('produces different location objects', (done) => {
+    it('produces different location objects', done => {
       EncodedReservedCharacters(history, done);
     });
   });
 
   describe('back', () => {
-    it('calls change listeners with the previous location', (done) => {
+    it('calls change listeners with the previous location', done => {
       GoBack(history, done);
     });
   });
 
   describe('forward', () => {
-    it('calls change listeners with the next location', (done) => {
+    it('calls change listeners with the next location', done => {
       GoForward(history, done);
     });
   });
 
   describe('block', () => {
-    it('blocks all transitions', (done) => {
+    it('blocks all transitions', done => {
       BlockEverything(history, done);
     });
   });
 
   describe('block a POP without listening', () => {
-    it('receives the next location and action as arguments', (done) => {
+    it('receives the next location and action as arguments', done => {
       BlockPopWithoutListening(history, done);
     });
   });
@@ -149,7 +149,7 @@ describe('a memory history with some initial entries', () => {
   it('clamps the initial index to a valid value', () => {
     let history = createMemoryHistory({
       initialEntries: ['/one', '/two', '/three'],
-      initialIndex: 3, // invalid
+      initialIndex: 3 // invalid
     });
 
     expect(history.index).toBe(2);
@@ -157,7 +157,7 @@ describe('a memory history with some initial entries', () => {
 
   it('starts at the last entry by default', () => {
     let history = createMemoryHistory({
-      initialEntries: ['/one', '/two', '/three'],
+      initialEntries: ['/one', '/two', '/three']
     });
 
     expect(history.index).toBe(2);
@@ -167,7 +167,7 @@ describe('a memory history with some initial entries', () => {
       query: {},
       hash: '',
       state: null,
-      key: expect.any(String),
+      key: expect.any(String)
     });
   });
 });
